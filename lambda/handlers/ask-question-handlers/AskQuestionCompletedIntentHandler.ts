5ccfe95765d60d9b2fa2aee61dc1fdc0e7bfe9f5
bryanleemoore <bryanmoore63b@gmail.com>
2023-03-01 00:29:33 -0800

    feat: add ability to track user token usage

--- lambda/handlers/ask-question-handlers/AskQuestionCompletedIntentHandler.ts
+++ lambda/handlers/ask-question-handlers/AskQuestionCompletedIntentHandler.ts
@@ -9,17 +9,28 @@ export const AskQuestionCompletedIntentHandler = {
     },
     async handle(handlerInput) {
         let question = handlerInput.requestEnvelope.request.intent.slots.question.value
-
-        const speakOutput = await API.getResponse(question)
-        if (speakOutput === false) {
+        const response = await API.getResponse(question)
+        if (response instanceof Error) {
             return handlerInput.responseBuilder
-                .speak('Sorry, there was a problem with OpenAPI. Please try again at a later time.')
+                .speak('Sorry, there was a problem with the OpenAPI API. Please try again at a later time.')
                 .withShouldEndSession(true)
                 .getResponse();
         }
+        const attributesManager = handlerInput.attributesManager;
+        const attributes = await attributesManager.getPersistentAttributes();
+
+        attributes.totalPromptTokensUsed += response.usage.prompt_tokens
+        attributes.totalCompletionTokensUsed += response.usage.completion_tokens
+        attributes.totalTokensUsed += response.usage.total_tokens
+
+        attributesManager.setPersistentAttributes(attributes);
+        await attributesManager.savePersistentAttributes();
+
+        let speakOutput = response.choices[0].text
         return handlerInput.responseBuilder
             .speak(speakOutput + ' Would you like to ask another question?')
             .reprompt(speakOutput)
             .getResponse();
+
     }
 };
